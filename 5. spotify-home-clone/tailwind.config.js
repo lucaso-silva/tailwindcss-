678b6b43f66b3858ed8ba2202f3843ceabeb5014
lucaso-silva <luc.osilva@gmail.com>
2023-12-12 20:44:04 -0800

    spotify-clone

---
+++ 5. spotify-home-clone/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      spacing: {
+        '125px': '125px'
+      },
+      colors: {
+        'hoverspt': '#18D760'
+      }
+    },
+  },
+  plugins: [],
+}
+
