990e277d44f216f2752a5db8be0fa8ab0aca1e16
lucaso-silva <luc.osilva@gmail.com>
2023-12-13 19:48:52 -0800

    build: main section - spotify clone

--- 5. spotify-home-clone/tailwind.config.js
+++ 5. spotify-home-clone/tailwind.config.js
@@ -4,10 +4,28 @@ module.exports = {
   theme: {
     extend: {
       spacing: {
-        '125px': '125px'
+        '125px': '125px',
+        '14': '3.5rem'
       },
       colors: {
-        'hoverspt': '#18D760'
+        'hoverspt': '#18D760',
+        'purple-main': '#2D46B9',
+        'green-main': '#1ED760'
+      },
+      backgroundImage: _theme => ({
+        'spotify-theme': "url('./src/img/bursts.svg')",
+        'spotify-theme-mobile': "url('./src/img/bursts-mobile.svg')"
+      }),
+      backgroundSize: {
+        '175%': '175%',
+        '195%': '195%'
+      },
+      backgroundPosition: {
+        'banner': '46% 4%',
+        'banner-mobile': 'top 25% center'
+      },
+      fontSize: {
+        '9xl': '9rem'
       }
     },
   },
