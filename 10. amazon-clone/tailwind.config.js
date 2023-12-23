1f23dca329a186d1b56013e5e46e4a0b802a0f8a
lucaso-silva <luc.osilva@gmail.com>
2023-12-22 21:54:18 -0800

    header - amazon

--- 10. amazon-clone/tailwind.config.js
+++ 10. amazon-clone/tailwind.config.js
@@ -4,8 +4,12 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+        'dark-blue-500': '#485769',
         'dark-blue-600': '#37475A',
+        'dark-blue-700': '#232F3E',
         'darkblue-900': '#191E26',
+        'dark-blue-950': '#131A22',
+        'orange-650': '#E47911',
         'gold-100': '#FEBD69',
         'gold-200': '#FAA63A'
       }
