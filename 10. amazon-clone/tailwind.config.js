73c22b4c0b69bbf727c41c9648a1a25cbcbcdfcf
lucaso-silva <luc.osilva@gmail.com>
2023-12-26 20:22:34 -0800

    (wip) footer - amazon

--- 10. amazon-clone/tailwind.config.js
+++ 10. amazon-clone/tailwind.config.js
@@ -7,11 +7,11 @@ module.exports = {
         'arial': 'Arial'
       },
       colors: {
-        'dark-blue-500': '#485769',
-        'dark-blue-600': '#37475A',
-        'dark-blue-700': '#232F3E',
+        'darkblue-500': '#485769',
+        'darkblue-600': '#37475A',
+        'darkblue-700': '#232F3E',
         'darkblue-900': '#191E26',
-        'dark-blue-950': '#131A22',
+        'darkblue-950': '#131A22',
         'orange-650': '#E47911',
         'gold-100': '#FEBD69',
         'gold-200': '#FAA63A'
